265f25bfb858d7b92625d5e5b39fa1ff679f9e25
sameh <sameh.elolimy@gmail.com>
2024-08-05 04:52:37 -0700

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -19,8 +19,8 @@ class BasicCache(BaseCaching):
         if key is not None and item is not None:
             self.cache_data[key] = item
 
-	def get(self, key):
-	'''return the value in `self.cache_data` linked to `key`
-	'''
+    def get(self, key):
+        '''return the value in `self.cache_data` linked to `key`
+        '''
 
-	return self.cache_data.get(key, None)
+        return self.cache_data.get(key, None)
